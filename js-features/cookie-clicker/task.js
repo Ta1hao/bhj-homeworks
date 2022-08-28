f89bf8269e68a083fa7b935bc91df9157082deda
Антон Рябов <nofacenoname3@yandex.ru>
2022-08-28 13:18:47 +0400

    second commit cookie-clicker task added ternary operator

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,11 +4,13 @@ const img = document.querySelector("img");
 const clickCounter = document.getElementById("clicker__counter");
 let click = (clickCounter.textContent = 0);
 img.onclick = function () {
-  if (img.width === 200) {
+ /* if (img.width === 200) {
     img.width = 300;
     clickCounter.textContent = ++click;
   } else if (img.width !== 200) {
     img.width = 200;
     clickCounter.textContent = ++click;
-  }
+  } можно сократить до тернарного оператора*/
+  img.width = img.width === 200 ? 300 : 200;
+clickCounter.textContent = ++click;
 };
